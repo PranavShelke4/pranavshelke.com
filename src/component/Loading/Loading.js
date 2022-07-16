bf73e0c9efc0cec25209f9299aa387692add7290
Pranav <pranavshelke4@gmail.com>
2022-07-16 23:27:38 +0530

    Loading Page Done For PC

---
+++ src/component/Loading/Loading.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Loading.css";
+import bgVideo from "../../assets/video/loading.mp4";
+
+function Loading() {
+  return (
+    <>
+      <video className="videoTag" autoPlay loop muted>
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      <div className="loadingIcon">
+        <div className="spinner-box">
+          <div className="blue-orbit leo"></div>
+
+          <div className="green-orbit leo"></div>
+
+          <div className="red-orbit leo"></div>
+
+          <div className="white-orbit w1 leo"></div>
+          <div className="white-orbit w2 leo"></div>
+          <div className="white-orbit w3 leo"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Loading;
---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loading from '../component/Loading/Loading'
+
+function LoadingPage() {
+  return (
+    <>
+      <Loading />
+    </>
+  )
+}
+
+export default LoadingPage
