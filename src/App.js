30a6843e75213faf46c2c539505bcd567373ebda
Pranav <pranavshelke4@gmail.com>
2022-07-17 14:20:27 +0530

    loading screen time set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
 import Loading from './pages/LoadingPage';
+import Home from './pages/HomePage';
+
 
 function App() {
+
+  const [showHome, setShowHome] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowHome(true);
+    }, 6000);
+  }, []);
+
   return (
     <div className="App">
-      <Loading />
+       {showHome ? <Home /> : <Loading />}      
     </div>
   );
 }
