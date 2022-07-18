662ac613364d1725be5682c4ca3bf6c624da9792
Pranav <pranavshelke4@gmail.com>
2022-07-18 23:14:03 +0530

    gamer side done

--- src/component/Home/Header/Header.js
+++ src/component/Home/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import DeveloperImg from "../../../assets/illustrator/face1.png";
 import Gamer from "../../../assets/illustrator/face2.png";
+import AssassinsCreed from "../../../assets/illustrator/AssassinsCreed.png";
 
 function Header() {
   let devTitle = "<Developer/>";
@@ -13,14 +14,20 @@ function Header() {
 
   return (
     <div className="header row">
+      {/* Developer Side */}
        <h1 className="devTitle" >{devTitle}</h1>
        <p className="devDis">Full Stack web-developer who <br></br> write clean and efficient code</p>
        <span className="devSnap">{snap1}</span>
-        <span className="devSnap2">{snap2}</span>
-        <span className="devSnap3">{snap3}</span>
-        <span className="devSnap4">{snap4}</span>
-        <span className="devSnap5">{snap5}</span>
+       <span className="devSnap2">{snap2}</span>
+       <span className="devSnap3">{snap3}</span>
+       <span className="devSnap4">{snap4}</span>
+       <span className="devSnap5">{snap5}</span>
       <img src={DeveloperImg} className="developer" alt="developer" />
+
+      {/* Gamer Side */}
+      <h1 className="gameTitle">Gamer</h1>
+      <p className="gameDis">passionate about play<br></br> mobile,pc games</p>
+      <img src={AssassinsCreed} className="gameLogo" alt="Assassins Creed Logo" />
       <img src={Gamer} className="gamer" alt="gamer" />
      
     </div>
