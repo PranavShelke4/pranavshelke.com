caa89a14badf1d47e57f3e283df539f579f66567
Pranav <pranavshelke4@gmail.com>
2022-07-18 20:21:54 +0530

    icon change

--- src/component/Home/Navbar/Navbar.js
+++ src/component/Home/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
-import me from "../../../assets/illustrator/me1.png";
+import me from "../../../assets/illustrator/Gamer.svg";
 
 function MyNavbar() {
   return (
