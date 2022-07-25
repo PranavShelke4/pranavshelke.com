fc00f7e927d340f68c9f2d7d6221ec0fdd476f54
Pranav <pranavshelke4@gmail.com>
2022-07-25 23:46:41 +0530

    .

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import NotFound from '../assets/illustrator/404.png'
+import React from "react";
+import NotFound from "../assets/illustrator/404.png";
 
 function PageNotFound() {
   return (
     <>
-        <img className='NotFoundImg' style={{ width: '80%', justifyItems: 'center', margin: '1%' }} src={NotFound} alt="Page Not Found"/>
+      <img
+        className="NotFoundImg"
+        style={{ width: "80%", 
+                 justifyItems: "center", 
+                 margin: "1%",
+                 }}
+        src={NotFound}
+        alt="Page Not Found"
+      />
+      
     </>
-  )
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
