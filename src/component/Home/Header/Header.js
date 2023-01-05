a51c37a9023bb7edb6198ad44141230f4f566f11
PranavShelke4 <pranavshelke4@gmail.com>
2023-01-05 17:24:20 +0530

    portfolio section some par done

--- src/component/Home/Header/Header.js
+++ src/component/Home/Header/Header.js
@@ -54,16 +54,12 @@ function Header() {
     </div>
     <p className="short-line"><br /><span>Front End Developer / WordPress Expert</span><br /></p>
     <Link to={"/"}><button className="contact-btn">Contact me!</button></Link>
-    <div class="scroll-down">
-            <span>scroll down</span>
-            
-        </div>
-
-        <div class="scroll-down scroll-down--left">
-            <span>scroll down</span>
-            
-        </div>
+    <div class="scrolldown" style="--color: skyblue">
+    <div class="chevrons">
+        <div class="chevrondown"></div>
+        <div class="chevrondown"></div>
     </div>
+</div>
     </>
   );
 }
--- src/component/Home/Portfolio/portfolio.js
+++ src/component/Home/Portfolio/portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./portfolio.css";
+import { Link } from "react-router-dom";
 
 function portfolio() {
   return (
@@ -8,6 +9,17 @@ function portfolio() {
       <div className="portfolio-tag1">
         <h1 className="portfolio-title">My Portfolio</h1>
       </div>
+      <div>
+        <p
+          className="portfolio-tag2"
+          style={{ position: "absolute", zIndex: "100", maxWidth: "60rem" }}
+        >
+          <br />A small gallery of recent projects chosen by me. I've done them
+          all together with amazing people from companies around the globe. It's
+          only a drop in the ocean compared to the entire list.<br />
+        </p>
+      </div>
+      <Link to={"/"}><button className="view-more-btn">See More</button></Link>
     </div>
   );
 }
